4f4625c4162981df5a59c25d320536b6fef9bb2b
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-03-02 22:45:09 +1000

    Create Calculator.cs

---
+++ WindowsFormsApp1/WindowsFormsApp1/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class Calculator
+    {
+        static public long Sum(long first, long second)
+        {
+            return first + second;
+        }
+        static public long Minus(long first, long second)
+        {
+            return first - second;
+        }
+        static public long Multi(long first, long second)
+        {
+            return first * second;
+        }
+        static public long Div(long first, long second)
+        {
+            long result;
+            if (second == 0)
+            {
+                result = 0;
+            }
+            else
+            {
+                result = first / second;
+            }
+            return result;
+        }
+        static public long Pow(long first, long second)
+        {
+            return Convert.ToInt64(Math.Pow(first, second));
+        }
+        static public long Root(long first)
+        {
+            return Convert.ToInt64(Math.Sqrt(first));
+        }
+    }
+}
