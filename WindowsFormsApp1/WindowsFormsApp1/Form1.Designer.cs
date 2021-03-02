1bfb77563e41800d3066bdcc2bf4e7062828b67d
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-03-02 22:45:04 +1000

    Create Form1.Designer.cs

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.but2 = new System.Windows.Forms.Button();
+            this.but7 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.but1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.but8 = new System.Windows.Forms.Button();
+            this.but5 = new System.Windows.Forms.Button();
+            this.but9 = new System.Windows.Forms.Button();
+            this.but0 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.but_del = new System.Windows.Forms.Button();
+            this.but6 = new System.Windows.Forms.Button();
+            this.but_sqrt = new System.Windows.Forms.Button();
+            this.but_row = new System.Windows.Forms.Button();
+            this.but_plus = new System.Windows.Forms.Button();
+            this.but_minus = new System.Windows.Forms.Button();
+            this.but_share = new System.Windows.Forms.Button();
+            this.but_multi = new System.Windows.Forms.Button();
+            this.but_aqually = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.but_m = new System.Windows.Forms.Button();
+            this.but_c = new System.Windows.Forms.Button();
+            this.but_mc = new System.Windows.Forms.Button();
+            this.but_mk = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // but2
+            // 
+            this.but2.Location = new System.Drawing.Point(94, 270);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(58, 46);
+            this.but2.TabIndex = 1;
+            this.but2.Text = "2";
+            this.but2.UseVisualStyleBackColor = true;
+            this.but2.Click += new System.EventHandler(this.but2_Click);
+            // 
+            // but7
+            // 
+            this.but7.Location = new System.Drawing.Point(30, 166);
+            this.but7.Name = "but7";
+            this.but7.Size = new System.Drawing.Size(58, 46);
+            this.but7.TabIndex = 2;
+            this.but7.Text = "7";
+            this.but7.UseVisualStyleBackColor = true;
+            this.but7.Click += new System.EventHandler(this.but7_Click);
+            // 
+            // but4
+            // 
+            this.but4.Location = new System.Drawing.Point(30, 218);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(58, 46);
+            this.but4.TabIndex = 3;
+            this.but4.Text = "4";
+            this.but4.UseVisualStyleBackColor = true;
+            this.but4.Click += new System.EventHandler(this.but4_Click);
+            // 
+            // but1
+            // 
+            this.but1.Location = new System.Drawing.Point(30, 270);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(58, 46);
+            this.but1.TabIndex = 4;
+            this.but1.Text = "1";
+            this.but1.UseVisualStyleBackColor = true;
+            this.but1.Click += new System.EventHandler(this.but1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(-110, 52);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // but8
+            // 
+            this.but8.Location = new System.Drawing.Point(94, 166);
+            this.but8.Name = "but8";
+            this.but8.Size = new System.Drawing.Size(58, 46);
+            this.but8.TabIndex = 6;
+            this.but8.Text = "8";
+            this.but8.UseVisualStyleBackColor = true;
+            this.but8.Click += new System.EventHandler(this.but8_Click);
+            // 
+            // but5
+            // 
+            this.but5.Location = new System.Drawing.Point(94, 218);
+            this.but5.Name = "but5";
+            this.but5.Size = new System.Drawing.Size(58, 46);
+            this.but5.TabIndex = 7;
+            this.but5.Text = "5";
+            this.but5.UseVisualStyleBackColor = true;
+            this.but5.Click += new System.EventHandler(this.but5_Click);
+            // 
+            // but9
+            // 
+            this.but9.Location = new System.Drawing.Point(158, 166);
+            this.but9.Name = "but9";
+            this.but9.Size = new System.Drawing.Size(58, 46);
+            this.but9.TabIndex = 8;
+            this.but9.Text = "9";
+            this.but9.UseVisualStyleBackColor = true;
+            this.but9.Click += new System.EventHandler(this.but9_Click);
+            // 
+            // but0
+            // 
+            this.but0.Location = new System.Drawing.Point(94, 322);
+            this.but0.Name = "but0";
+            this.but0.Size = new System.Drawing.Size(58, 46);
+            this.but0.TabIndex = 9;
+            this.but0.Text = "0";
+            this.but0.UseVisualStyleBackColor = true;
+            this.but0.Click += new System.EventHandler(this.but0_Click);
+            // 
+            // but3
+            // 
+            this.but3.Location = new System.Drawing.Point(158, 270);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(58, 46);
+            this.but3.TabIndex = 10;
+            this.but3.Text = "3";
+            this.but3.UseVisualStyleBackColor = true;
+            this.but3.Click += new System.EventHandler(this.but3_Click);
+            // 
+            // but_del
+            // 
+            this.but_del.Location = new System.Drawing.Point(222, 115);
+            this.but_del.Name = "but_del";
+            this.but_del.Size = new System.Drawing.Size(58, 45);
+            this.but_del.TabIndex = 11;
+            this.but_del.Text = "del";
+            this.but_del.UseVisualStyleBackColor = true;
+            this.but_del.Click += new System.EventHandler(this.but_del_Click);
+            // 
+            // but6
+            // 
+            this.but6.Location = new System.Drawing.Point(158, 218);
+            this.but6.Name = "but6";
+            this.but6.Size = new System.Drawing.Size(58, 46);
+            this.but6.TabIndex = 12;
+            this.but6.Text = "6";
+            this.but6.UseVisualStyleBackColor = true;
+            this.but6.Click += new System.EventHandler(this.but6_Click);
+            // 
+            // but_sqrt
+            // 
+            this.but_sqrt.Location = new System.Drawing.Point(286, 163);
+            this.but_sqrt.Name = "but_sqrt";
+            this.but_sqrt.Size = new System.Drawing.Size(58, 46);
+            this.but_sqrt.TabIndex = 13;
+            this.but_sqrt.Text = "sqrt";
+            this.but_sqrt.UseVisualStyleBackColor = true;
+            this.but_sqrt.Click += new System.EventHandler(this.but_sqrt_Click);
+            // 
+            // but_row
+            // 
+            this.but_row.Location = new System.Drawing.Point(286, 215);
+            this.but_row.Name = "but_row";
+            this.but_row.Size = new System.Drawing.Size(58, 46);
+            this.but_row.TabIndex = 14;
+            this.but_row.Text = "pow";
+            this.but_row.UseVisualStyleBackColor = true;
+            this.but_row.Click += new System.EventHandler(this.but_row_Click);
+            // 
+            // but_plus
+            // 
+            this.but_plus.Location = new System.Drawing.Point(222, 322);
+            this.but_plus.Name = "but_plus";
+            this.but_plus.Size = new System.Drawing.Size(58, 46);
+            this.but_plus.TabIndex = 15;
+            this.but_plus.Text = "+";
+            this.but_plus.UseVisualStyleBackColor = true;
+            this.but_plus.Click += new System.EventHandler(this.but_plus_Click);
+            // 
+            // but_minus
+            // 
+            this.but_minus.Location = new System.Drawing.Point(222, 270);
+            this.but_minus.Name = "but_minus";
+            this.but_minus.Size = new System.Drawing.Size(58, 46);
+            this.but_minus.TabIndex = 16;
+            this.but_minus.Text = "-";
+            this.but_minus.UseVisualStyleBackColor = true;
+            this.but_minus.Click += new System.EventHandler(this.but_minus_Click);
+            // 
+            // but_share
+            // 
+            this.but_share.Location = new System.Drawing.Point(222, 218);
+            this.but_share.Name = "but_share";
+            this.but_share.Size = new System.Drawing.Size(58, 46);
+            this.but_share.TabIndex = 17;
+            this.but_share.Text = "/";
+            this.but_share.UseVisualStyleBackColor = true;
+            this.but_share.Click += new System.EventHandler(this.but_share_Click);
+            // 
+            // but_multi
+            // 
+            this.but_multi.Location = new System.Drawing.Point(222, 166);
+            this.but_multi.Name = "but_multi";
+            this.but_multi.Size = new System.Drawing.Size(58, 46);
+            this.but_multi.TabIndex = 18;
+            this.but_multi.Text = "*";
+            this.but_multi.UseVisualStyleBackColor = true;
+            this.but_multi.Click += new System.EventHandler(this.but_multi_Click);
+            // 
+            // but_aqually
+            // 
+            this.but_aqually.Location = new System.Drawing.Point(286, 270);
+            this.but_aqually.Name = "but_aqually";
+            this.but_aqually.Size = new System.Drawing.Size(58, 98);
+            this.but_aqually.TabIndex = 20;
+            this.but_aqually.Text = "=";
+            this.but_aqually.UseVisualStyleBackColor = true;
+            this.but_aqually.Click += new System.EventHandler(this.but_qually_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(88, -67);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(0, 20);
+            this.linkLabel1.TabIndex = 21;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 27);
+            this.textBox1.TabIndex = 22;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // but_m
+            // 
+            this.but_m.Location = new System.Drawing.Point(30, 115);
+            this.but_m.Name = "but_m";
+            this.but_m.Size = new System.Drawing.Size(58, 45);
+            this.but_m.TabIndex = 23;
+            this.but_m.Text = "m";
+            this.but_m.UseVisualStyleBackColor = true;
+            this.but_m.Click += new System.EventHandler(this.but_m_Click);
+            // 
+            // but_c
+            // 
+            this.but_c.Location = new System.Drawing.Point(286, 115);
+            this.but_c.Name = "but_c";
+            this.but_c.Size = new System.Drawing.Size(58, 45);
+            this.but_c.TabIndex = 25;
+            this.but_c.Text = "c";
+            this.but_c.UseVisualStyleBackColor = true;
+            this.but_c.Click += new System.EventHandler(this.but_c_Click);
+            // 
+            // but_mc
+            // 
+            this.but_mc.Location = new System.Drawing.Point(158, 115);
+            this.but_mc.Name = "but_mc";
+            this.but_mc.Size = new System.Drawing.Size(58, 45);
+            this.but_mc.TabIndex = 26;
+            this.but_mc.Text = "mc";
+            this.but_mc.UseVisualStyleBackColor = true;
+            this.but_mc.Click += new System.EventHandler(this.but_mc_Click);
+            // 
+            // but_mk
+            // 
+            this.but_mk.Location = new System.Drawing.Point(94, 115);
+            this.but_mk.Name = "but_mk";
+            this.but_mk.Size = new System.Drawing.Size(58, 45);
+            this.but_mk.TabIndex = 27;
+            this.but_mk.Text = "mk";
+            this.but_mk.UseVisualStyleBackColor = true;
+            this.but_mk.Click += new System.EventHandler(this.but_mk_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(222, 29);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 27);
+            this.textBox2.TabIndex = 28;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 418);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.but_mk);
+            this.Controls.Add(this.but_mc);
+            this.Controls.Add(this.but_c);
+            this.Controls.Add(this.but_m);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.but_aqually);
+            this.Controls.Add(this.but_multi);
+            this.Controls.Add(this.but_share);
+            this.Controls.Add(this.but_minus);
+            this.Controls.Add(this.but_plus);
+            this.Controls.Add(this.but_row);
+            this.Controls.Add(this.but_sqrt);
+            this.Controls.Add(this.but6);
+            this.Controls.Add(this.but_del);
+            this.Controls.Add(this.but3);
+            this.Controls.Add(this.but0);
+            this.Controls.Add(this.but9);
+            this.Controls.Add(this.but5);
+            this.Controls.Add(this.but8);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.but1);
+            this.Controls.Add(this.but4);
+            this.Controls.Add(this.but7);
+            this.Controls.Add(this.but2);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.Button but7;
+        private System.Windows.Forms.Button but4;
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button but8;
+        private System.Windows.Forms.Button but5;
+        private System.Windows.Forms.Button but9;
+        private System.Windows.Forms.Button but0;
+        private System.Windows.Forms.Button but3;
+        private System.Windows.Forms.Button but_del;
+        private System.Windows.Forms.Button but6;
+        private System.Windows.Forms.Button but_sqrt;
+        private System.Windows.Forms.Button but_row;
+        private System.Windows.Forms.Button but_plus;
+        private System.Windows.Forms.Button but_minus;
+        private System.Windows.Forms.Button but_share;
+        private System.Windows.Forms.Button but_multi;
+        private System.Windows.Forms.Button but_aqually;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button but_m;
+        private System.Windows.Forms.Button but_c;
+        private System.Windows.Forms.Button but_mc;
+        private System.Windows.Forms.Button but_mk;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
