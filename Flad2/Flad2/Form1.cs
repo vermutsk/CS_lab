47a03eecc66a6f0847e41086150e32eff4f4be25
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-04-16 13:04:00 +1000

    Update Form1.cs

--- Flad2/Flad2/Form1.cs
+++ Flad2/Flad2/Form1.cs
@@ -12,7 +12,6 @@ namespace Flad2
 {
     public partial class Form1 : Form
     {
-        int line_counter = 0;
         private Point startCoord;
         private Point endCoord;
         private int animationValue;
@@ -29,33 +28,34 @@ namespace Flad2
         {
             animationValue += 1;
             CalculateFlag();
-            this.drawPanel.Invalidate();
+            this.drawPanel.Refresh();
         }
         private void mouseDown(object sender, MouseEventArgs e)
         {
             this.drawPanel.MouseMove += mouseMove;
             startCoord = e.Location;
             endCoord = e.Location;
-            // timer1.Stop();
+            timer1.Stop();
         }
 
         private void mouseUp(object sender, MouseEventArgs e)
         {
-            //this.MouseMove -= mouseMove;
+            this.MouseMove -= mouseMove;
             this.drawPanel.MouseMove -= mouseMove;
-            // timer1.Start();
+            timer1.Start();
         }
 
         private void mouseMove(object sender, MouseEventArgs e)
         {
             CalculateFlag();
-            this.drawPanel.Invalidate();
             endCoord = e.Location;
-
+            this.drawPanel.Invalidate();
         }
         private void paint(object sender, PaintEventArgs e)
         {
             DrawFlag(startCoord, endCoord);
+            //Graphics qw = drawPanel.CreateGraphics();
+            //qw.DrawLine(new Pen(new SolidBrush(Color.Black)), 0, 0, 200, 200);
         }
 
         private void CalculateFlag()
@@ -71,64 +71,38 @@ namespace Flad2
             }
         }
 
-        void Show(string message)
-        {
-            line_counter += 1;
-            logBox.Text += String.Format("{0}: {1}\r\n", line_counter, message);
-            logBox.SelectionStart = logBox.Text.Length;
-            logBox.ScrollToCaret();
-
-        }
-
         private void DrawFlag(Point startCoord, Point endCoord)
         {
-            Show(String.Format("DrawFlag({0}, {1}, {2})", startCoord, endCoord, isCreated));
+            //Show(String.Format("DrawFlag({0}, {1}, {2})", startCoord, endCoord, isCreated));
             if (!isCreated)
             {
                 return;
             }
             int n = 0;
-            if (startCoord.Y <= endCoord.Y)
+            List<Color> colors = new List<Color>()
+                { Color.Red, Color.Orange, Color.Yellow, Color.LimeGreen, Color.DeepSkyBlue, Color.Blue, Color.DarkViolet };
+            if (startCoord.Y >= endCoord.Y)
             {
-                List<Color> colors = new List<Color>()
-                { Color.Red, Color.Orange, Color.Yellow, Color.Green, Color.LightBlue, Color.Blue, Color.Violet };
-
-                Show(String.Format("{0}:{1}", colors.Count, polygons.Length));
-                /*foreach (var item in colors)
-                {
-                    Show(String.Format("{0}", item.Name));
-                }*/
-                /*foreach (var row in polygons)
-                {
-                    foreach (var polygons in row)
-                    {
-                            Show(String.Format("{0}", polygons[0]));
-                    }
-                }*/
-
-
-                foreach (Color i in colors)
-                {
-                    DrawLine(new SolidBrush(i), polygons[n]);
-                    n++;
-                }
+                colors.Reverse();
             }
-            else
+            foreach (Color i in colors)
             {
-                List<Color> colors = new List<Color>()
-                {Color.Violet, Color.Blue, Color.LightBlue, Color.Green, Color.Yellow, Color.Orange, Color.Red};
-
-                foreach (Color i in colors)
-                {
-                    DrawLine(new SolidBrush(i), polygons[n]);
-                    n++;
-                }
-            }
-            /*foreach (Color i in colors)
-            {
-                DrawLine(new SolidBrush(i), polygons[n]);
+                DrawLine(new SolidBrush(i), polygons[n]);   
                 n++;
-            }*/
+            }
+        }
+        private void DrawLine(SolidBrush brush, PointF[][] polygon)
+        {
+            Graphics formGraphics;
+            formGraphics = this.drawPanel.CreateGraphics();
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            path.AddCurve(polygon[0], tension: 4.5f);
+            path.AddLine(polygon[1][0], polygon[1][1]);
+            path.AddCurve(polygon[2], tension: 4.5f);
+            path.AddLine(polygon[3][0], polygon[3][1]);
+            formGraphics.FillPath(brush, path);
+            brush.Dispose();
+            formGraphics.Dispose();
 
         }
         private PointF [][] CreatePolygon(int lenWight, int lenHeight, int startY)
@@ -158,20 +132,5 @@ namespace Flad2
 
             return polygon;
         }
-
-        private void DrawLine(SolidBrush brush, PointF[][] polygon)
-        {
-            System.Drawing.Graphics formGraphics;
-            formGraphics = this.drawPanel.CreateGraphics();
-            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
-            path.AddCurve(polygon[0], tension: 4.5f);
-            path.AddLine(polygon[1][0], polygon[1][1]);
-            path.AddCurve(polygon[2], tension: 4.5f);
-            path.AddLine(polygon[3][0], polygon[3][1]);
-            formGraphics.FillPath(brush, path);
-            brush.Dispose();
-            formGraphics.Dispose();
-        }
-
     }
 }
