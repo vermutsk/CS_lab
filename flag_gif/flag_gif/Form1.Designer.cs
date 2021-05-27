3ae4a056045ca499098cbbd3ecbc168c755d376b
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-05-27 14:53:50 +1000

    Create Form1.Designer.cs

---
+++ flag_gif/flag_gif/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace flag_gif
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.drawPanel = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.drawPanel)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawPanel
+            // 
+            this.drawPanel.BackColor = System.Drawing.Color.GhostWhite;
+            this.drawPanel.Location = new System.Drawing.Point(12, 12);
+            this.drawPanel.Name = "drawPanel";
+            this.drawPanel.Size = new System.Drawing.Size(805, 390);
+            this.drawPanel.TabIndex = 0;
+            this.drawPanel.TabStop = false;
+            this.drawPanel.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.drawPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.PaintForm);
+            this.drawPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.start);
+            this.drawPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.paint);
+            this.drawPanel.MouseUp += new System.Windows.Forms.MouseEventHandler(this.end);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(829, 484);
+            this.Controls.Add(this.drawPanel);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "CreateYourFlag";
+            ((System.ComponentModel.ISupportInitialize)(this.drawPanel)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox drawPanel;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
