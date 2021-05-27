e401e996fe83275ec565fb560f93d4a0e051dd03
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-05-27 14:53:24 +1000

    Create Load.cs

---
+++ Flad2/Flad2/Load.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Load
+{
+    public partial class Load : Form
+    {
+        public Load()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (loadBar.Value == loadBar.Maximum)
+            {
+                timer1.Stop();
+                this.Close();
+            }
+            loadBar.PerformStep();
+        }
+
+        private void Load_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
