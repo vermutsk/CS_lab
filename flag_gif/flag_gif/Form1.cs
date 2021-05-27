8a4c84528f731101fc4508aeaf086e0e0ac27657
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-05-27 14:53:38 +1000

    Create Form1.cs

---
+++ flag_gif/flag_gif/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flag_gif
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Rectangle RcDraw;
+        private Point startCoord = new Point();
+        private Point endCoord = new Point();
+
+
+        private void start(object sender, MouseEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void end(object sender, MouseEventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void PaintForm(object sender, PaintEventArgs e)
+        {
+            var redBrash = new SolidBrush(Color.Red);
+            var formGrafics = this.drawPanel.CreateGraphics();
+            //formGrafics.FillPath(redBrash, path);
+            
+        }
+        private void CalculateFlag()
+        { 
+
+        }
+    }
+
+    
+}
