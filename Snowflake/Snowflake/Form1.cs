91f3a3c579e3f48f65d6bca8ec847b51c4be2c04
vermutsk <62577988+vermutsk@users.noreply.github.com>
2021-06-20 16:17:07 +1000

    Update Form1.cs

--- Snowflake/Snowflake/Form1.cs
+++ Snowflake/Snowflake/Form1.cs
@@ -13,16 +13,14 @@ namespace Snowflake
     public partial class Form1 : Form
     {
         private int n = -1;
-        private int memory = -1;
         private Pen pen1;
         private Graphics g;
         private Pen pen2;
-        private bool create = false;
-        private bool color = true;
+        bool memory = true;
         private PointF point1 = new PointF(150, 200);
         private PointF point2 = new PointF(750, 200);
         private PointF point3 = new PointF(450, 700);
-        private PointF[][] levels = new PointF[5][];
+        
         public Form1()
         {
             InitializeComponent();
@@ -30,197 +28,60 @@ namespace Snowflake
 
         private void Back_Click(object sender, EventArgs e)
         {
-            if (n > 0)
-            {
-                n--;
-                color = false;
-                Fractal();
-            }
-            
+            n--;
+            g.Clear(Color.MintCream);
+            Fractal(point1, point2, point3, n);
+            Fractal(point2, point3, point1, n);
+            Fractal(point3, point1, point2, n);
+            memory = true;
         }
 
         private void Next_Click(object sender, EventArgs e)
         {
-            if (n < 5)
-            {
-                n++;
-                color = true;
-                if (n > memory)
-                {
-                    memory++;
-                }
-                if (memory > n)
-                {
-                    Draw();
-                }
-                else
-                {
-                    Fractal();
-                }
-            }
-            
+            n++;
+            Fractal(point1, point2, point3, n);
+            Fractal(point2, point3, point1, n);
+            Fractal(point3, point1, point2, n);
+            memory = true;
         }
-        private void Draw()
-        {
-            g = panel1.CreateGraphics();
-            
-            //Выбираем цвета зарисовки 
-            pen1 = new Pen(Color.Green, 1);
-            List<Color> colors = new List<Color>()
-                { Color.Red, Color.Orange, Color.Yellow, Color.LimeGreen, Color.DeepSkyBlue, Color.Blue, Color.DarkViolet };
-            Random rnd = new Random();
-            int f = rnd.Next()%7;
-            pen1 = new Pen(colors[f], 1);
-            pen2 = new Pen(Color.MintCream, 2);
-            
-            //Определяем объект "g" класса Graphics 
-            if (color == true)
-            {
-                int i = 0;
-                try
-                {
-                    while (true)
-                    {
-                        if (i % 3 == 0)
-                        {
-                            f = rnd.Next() % 7;
-                            pen1 = new Pen(colors[f], 1);
-                        }
-
-                        var p4 = levels[n][i];
-                        var p5 = levels[n][i + 1];
-                        //координаты вершины угла
-                        var p6 = levels[n][i + 2];
-                        //Зарисуем треугольник
-                        g.DrawLine(pen1, p4, p5);
-                        g.DrawLine(pen1, p5, p6);
-                        if (create == true)
-                        {
-                            //g.DrawLine(pen2, p6, p4);
-                        }
-                        else
-                        {
-                            g.DrawLine(pen1, p6, p4);
-                            create = true;
-                        }
-                        i += 3;
-                    }
-                }
-                catch
-                {
-
-                }
-            }
-            else
-            {
-                int i = 0;
-                try
-                {
-                    while (true)
-                    {
-                        var p4 = levels[n+1][i];
-                        var p5 = levels[n+1][i + 1];
-                        //координаты вершины угла
-                        var p6 = levels[n+1][i + 2];
-                        //Зарисуем треугольник
-                        g.DrawLine(pen2, p4, p5);
-                        g.DrawLine(pen2, p5, p6);
-                        i += 3;
-                    }
-                }
-                catch
-                {
-
-                }
-            }
-                       
-        }
-
 
         //рекурсивная функция рисования кривой Коха
-        private void Fractal()
+        public int Fractal(PointF p1, PointF p2, PointF p3, int iter)
         {
-            if (create == false)
+            g = panel1.CreateGraphics();
+            pen1 = new Pen(Color.Green, 1);
+            pen2 = new Pen(Color.MintCream, 2);
+            if (n - iter == 0 & memory == true)
             {
-                levels[0] = new PointF[3];
-                levels[0][0] = point1;
-                levels[0][1] = point3;
-                levels[0][2] = point2;
-                Draw();
+                g.DrawLine(pen1, point1, point2);
+                g.DrawLine(pen1, point2, point3);
+                g.DrawLine(pen1, point3, point1);
+                memory = false;
             }
-            
-            int i = 0;
-            int limit;
-            if (n == 1)
+            //n -количество итераций
+            if (iter > 0)  //условие выхода из рекурсии
             {
-                limit = 3;
-            }
-            else
-            {
-                limit = 2;
-            }
-            try
-            {
-                int j = 0;
-                while (true)
-                {
-                    for (int z = 0; z < limit; z++)
-                    {
-                        var p1 = levels[n - 1][(i + z) % 3 + j];
-                        var p2 = levels[n - 1][(i + z + 1) % 3 + j];
-                        var p3 = levels[n - 1][(i + z + 2) % 3 + j];
-
-                        var p4 = new PointF((p2.X + 2 * p1.X) / 3, (p2.Y + 2 * p1.Y) / 3);
-                        var p5 = new PointF((2 * p2.X + p1.X) / 3, (p1.Y + 2 * p2.Y) / 3);
-                        //координаты вершины угла
-                        var ps = new PointF((p2.X + p1.X) / 2, (p2.Y + p1.Y) / 2);
-                        var pn = new PointF((4 * ps.X - p3.X) / 3, (4 * ps.Y - p3.Y) / 3);
-                        Array.Resize(ref levels[n], i + 3);
-                        levels[n][i] = p4;
-                        levels[n][i + 1] = pn;
-                        levels[n][i + 2] = p5;
-
-                        i += 3;
-                        Calculate(p4, pn, p5, i);
-                        i += 3;
-                        Calculate(pn, p5, p4, i);
-                        i += 3;
-                        Calculate(p1, p4, new PointF((2 * p1.X + p3.X) / 3, (2 * p1.Y + p3.Y) / 3), i);
-                        i += 3;
-                        Calculate(p5, p2, new PointF((2 * p2.X + p3.X) / 3, (2 * p2.Y + p3.Y) / 3), i);
-                        i += 3;
-                    }
-                    j += 3;
-                } 
-            }
-            catch
-            {
-            }
-                
-            Draw();
-
-        }
-        private void Calculate(PointF p1, PointF p2, PointF p3, int i)
-        {
-            try
-            {
-                
+                //средняя треть отрезка
                 var p4 = new PointF((p2.X + 2 * p1.X) / 3, (p2.Y + 2 * p1.Y) / 3);
                 var p5 = new PointF((2 * p2.X + p1.X) / 3, (p1.Y + 2 * p2.Y) / 3);
                 //координаты вершины угла
                 var ps = new PointF((p2.X + p1.X) / 2, (p2.Y + p1.Y) / 2);
                 var pn = new PointF((4 * ps.X - p3.X) / 3, (4 * ps.Y - p3.Y) / 3);
-                Array.Resize(ref levels[n], i + 3);
-                levels[n][i] = p4;
-                levels[n][i + 1] = pn;
-                levels[n][i + 2] = p5;
-                
-            }
-            catch
-            {
-            }
+                //рисуем его
+                g.DrawLine(pen1, p4, pn);
+                g.DrawLine(pen1, p5, pn);
+                g.DrawLine(pen2, p4, p5);
 
+
+                //рекурсивно вызываем функцию нужное число раз
+                Fractal(p4, pn, p5, iter - 1);
+                Fractal(pn, p5, p4, iter - 1);
+                Fractal(p1, p4, new PointF((2 * p1.X + p3.X) / 3, (2 * p1.Y + p3.Y) / 3), iter - 1);
+                Fractal(p5, p2, new PointF((2 * p2.X + p3.X) / 3, (2 * p2.Y + p3.Y) / 3), iter - 1);
+
+            }
+            return iter;
         }
-            
+
     }
 }
